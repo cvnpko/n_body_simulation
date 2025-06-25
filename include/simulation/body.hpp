ed6f834180a8e752737d8bd08052db7ee912fb19
cvnpko <lazar.igob@gmail.com>
2025-06-25 12:34:55 +0200

    feat: body class

---
+++ include/simulation/body.hpp
@@ -0,0 +1,24 @@
+#ifndef HPP_BODY
+#define HPP_BODY
+
+namespace sim
+{
+    class Body2d
+    {
+    public:
+        Body2d(double mass, double x, double y, double vx, double vy);
+
+    private:
+        double mass, x, y, vx, vy;
+    };
+    class Body3d
+    {
+    public:
+        Body3d(double mass, double x, double y, double z, double vx, double vy, double vz);
+
+    private:
+        double mass, x, y, z, vx, vy, vz;
+    };
+}
+
+#endif
--- src/simulation/body.cpp
+++ src/simulation/body.cpp
@@ -1,6 +1,8 @@
-namespace simulation
+#include "body.hpp"
+namespace sim
 {
-    class body
-    {
-    };
+    Body2d::Body2d(double mass, double x, double y, double vx, double vy)
+        : mass(mass), x(x), y(y), vx(vx), vy(vy) {}
+    Body3d::Body3d(double mass, double x, double y, double z, double vx, double vy, double vz)
+        : mass(mass), x(x), y(y), z(z), vx(vx), vy(vy), vz(vz) {}
 }
