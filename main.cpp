e4e8bcd96a4320df60be478ded40856ed140405a
cvnpko <lazar.igob@gmail.com>
2025-06-26 16:58:30 +0200

    feat: add menu

--- main.cpp
+++ main.cpp
@@ -1,6 +1,9 @@
 #include <glad/glad.h>
 #include <GLFW/glfw3.h>
 #include <iostream>
+#include "imgui.h"
+#include "imgui_impl_glfw.h"
+#include "imgui_impl_opengl3.h"
 
 void framebuffer_size_callback(GLFWwindow *window, int width, int height);
 void processInput(GLFWwindow *window);
@@ -31,12 +34,74 @@ int main()
         return 1;
     }
 
+    IMGUI_CHECKVERSION();
+    ImGui::CreateContext();
+    ImGuiStyle &style = ImGui::GetStyle();
+    style.FrameRounding = 20.0f;
+    ImGui::GetIO().FontGlobalScale = 2.0f;
+
+    ImGui_ImplGlfw_InitForOpenGL(window, true);
+    ImGui_ImplOpenGL3_Init("#version 330 core");
+
     while (!glfwWindowShouldClose(window))
     {
         processInput(window);
+        ImGui_ImplOpenGL3_NewFrame();
+        ImGui_ImplGlfw_NewFrame();
+        ImGui::NewFrame();
+
+        ImGuiIO &io = ImGui::GetIO();
+        ImVec2 window_size = ImVec2(io.DisplaySize.x / 2.0f, io.DisplaySize.y);
+        ImVec2 window_pos = ImVec2(
+            (io.DisplaySize.x - window_size.x) * 0.5f,
+            0.0f);
+
+        ImGui::SetNextWindowPos(window_pos, ImGuiCond_Always);
+        ImGui::SetNextWindowBgAlpha(0.0f);
+
+        ImGui::Begin("Kontrole", nullptr,
+                     ImGuiWindowFlags_NoDecoration |
+                         ImGuiWindowFlags_NoMove |
+                         ImGuiWindowFlags_NoSavedSettings |
+                         ImGuiWindowFlags_AlwaysAutoResize |
+                         ImGuiWindowFlags_NoBackground);
+        ImVec2 button_size = ImVec2(window_size.x, window_size.y / 6.0f);
+        ImVec2 dummy_size = ImVec2(window_size.x, window_size.y / 6.0f / 12.0f);
+        ImGui::Dummy(dummy_size);
+        if (ImGui::Button("Three bodies", button_size))
+        {
+        }
+        ImGui::Dummy(dummy_size);
+        if (ImGui::Button("Fixed 2 bodies", button_size))
+        {
+        }
+        ImGui::Dummy(dummy_size);
+        if (ImGui::Button("Small n bodies", button_size))
+        {
+        }
+        ImGui::Dummy(dummy_size);
+        if (ImGui::Button("Big n bodies", button_size))
+        {
+        }
+        ImGui::Dummy(dummy_size);
+        if (ImGui::Button("Three bodies 3D", button_size))
+        {
+        }
+
+        ImGui::End();
+
+        ImGui::Render();
+        glClearColor(0.45f, 0.55f, 0.60f, 1.00f);
+        glClear(GL_COLOR_BUFFER_BIT);
+        ImGui_ImplOpenGL3_RenderDrawData(ImGui::GetDrawData());
+
         glfwSwapBuffers(window);
         glfwPollEvents();
     }
+
+    ImGui_ImplOpenGL3_Shutdown();
+    ImGui_ImplGlfw_Shutdown();
+    ImGui::DestroyContext();
 
     glfwTerminate();
 
